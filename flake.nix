2c92ae59b2c40c80ea616ed24d2c7c239a91b37e
itstarsun <itstarsun@proton.me>
2023-03-09 17:12:39 +0000

    add apps output

--- flake.nix
+++ flake.nix
@@ -22,6 +22,23 @@
     // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
+
+        apps = [
+          "frida"
+          "frida-ls-devices"
+          "frida-ps"
+          "frida-kill"
+          "frida-ls"
+          "frida-rm"
+          "frida-pull"
+          "frida-push"
+          "frida-discover"
+          "frida-trace"
+          "frida-join"
+          "frida-create"
+          "frida-compile"
+          "frida-apk"
+        ];
       in
       {
         packages = {
@@ -31,6 +48,13 @@
             frida-gumjs;
           frida-python = pkgs.python3Packages.frida;
           frida-tools = pkgs.python3Packages.frida-tools;
+        };
+
+        apps = (nixpkgs.lib.genAttrs apps (name: flake-utils.lib.mkApp {
+          inherit name;
+          drv = self.packages.${system}.frida-tools;
+        })) // {
+          default = self.apps.${system}.frida;
         };
 
         devShells.default = pkgs.mkShellNoCC {
