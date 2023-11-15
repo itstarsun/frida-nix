bd1c9d6af0cf1ac11c32298910b71cf70ba0c069
itstarsun <itstarsun@proton.me>
2023-11-15 08:58:57 +0000

    frida-gum: enable gumjs

--- frida-gum.nix
+++ frida-gum.nix
@@ -1,5 +1,6 @@
 { lib
 , stdenv
+, callPackage
 , version
 , src
 
@@ -7,8 +8,15 @@
 , frida-toolchain
 , meson
 , ninja
+, nodejs
+, npmHooks
 , pkg-config
+, python3
 }:
+
+let
+  frida-gumjs-bindings = callPackage ./frida-gumjs-bindings { };
+in
 
 stdenv.mkDerivation {
   pname = "frida-gum";
@@ -16,6 +24,20 @@ stdenv.mkDerivation {
   sourceRoot = "${src.name}/frida-gum";
 
   outputs = [ "out" "bin" "dev" "lib" ];
+
+  postPatch = ''
+    patchShebangs .
+
+    mkdir -p build/bindings/gumjs
+    cp ${frida-gumjs-bindings}/package-lock.json build/bindings/gumjs
+  '';
+
+  npmRoot = "build/bindings/gumjs";
+  npmDeps = frida-gumjs-bindings;
+
+  disallowedReferences = [
+    frida-gumjs-bindings
+  ];
 
   buildInputs = [
     frida-sdk
@@ -25,11 +47,15 @@ stdenv.mkDerivation {
     frida-toolchain
     meson
     ninja
+    nodejs
+    npmHooks.npmConfigHook
     pkg-config
+    python3
   ];
 
   mesonFlags = [
     (lib.mesonOption "default_library" "static")
+    (lib.mesonEnable "gumjs" true)
     (lib.mesonEnable "gumpp" true)
   ];
 
