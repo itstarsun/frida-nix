4d56bd7e9d48f4518862a930941ee24ca52fa5b3
itstarsun <itstarsun@proton.me>
2023-11-15 08:34:46 +0000

    frida-sdk and frida-toolchain

---
+++ bundle.nix
@@ -0,0 +1,45 @@
+{ lib
+, stdenv
+, name
+, version
+, hash
+, fetchurl
+
+, autoPatchelfHook
+, python3
+}:
+
+let
+  isToolchian = name == "toolchain";
+
+  system = {
+    x86_64-linux = "linux-x86_64";
+  }.${stdenv.system};
+in
+
+stdenv.mkDerivation {
+  pname = "frida-${name}";
+  inherit version;
+
+  src = fetchurl {
+    url = "https://build.frida.re/deps/${version}/${name}-${system}.tar.bz2";
+    inherit hash;
+  };
+
+  dontUnpack = true;
+
+  buildInputs = lib.optionals isToolchian [
+    python3
+  ];
+
+  nativeBuildInputs = lib.optionals stdenv.isLinux [
+    autoPatchelfHook
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir $out
+    tar xf $src -C $out
+    runHook postInstall
+  '';
+}
--- default.nix
+++ default.nix
@@ -1,11 +1,40 @@
-{ lib, newScope, fetchFromGitHub }:
+{ lib
+, stdenv
+, newScope
+, fetchFromGitHub
+}:
 
-lib.makeScope newScope (self: {
+let
+  version = "16.1.6";
+  deps = {
+    version = "20230622";
+    bundles = {
+      x86_64-linux = {
+        sdk = "sha256-mM6hEMFQUfi4apeR4UVkNf2Jd4vanfHEyFLAADpx69E=";
+        toolchain = "sha256-6FaKEK8YYXnYA+bHk+QSNmud6q0Hac1BuLBQHoSfaFQ=";
+      };
+    };
+  };
+in
+
+lib.makeScope newScope (self: with self;
+let
+  mkFridaBundle = name:
+    callPackage ./bundle.nix {
+      inherit name;
+      inherit (deps) version;
+      hash = deps.bundles.${stdenv.system}.${name};
+    };
+in
+{
   src = fetchFromGitHub {
     owner = "frida";
     repo = "frida";
-    rev = "16.1.6";
+    rev = version;
     hash = "sha256-LLrcJ04wcn6CQ/8NzLdAz6LWyc/jaQgj67bIscMMtMA=";
     fetchSubmodules = true;
   };
+
+  frida-sdk = mkFridaBundle "sdk";
+  frida-toolchain = mkFridaBundle "toolchain";
 })
