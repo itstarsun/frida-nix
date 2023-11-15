9295645b1a3c39f95927b811ad5076d91a0a0159
itstarsun <itstarsun@proton.me>
2023-11-15 08:56:09 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+
+      eachSystem = f:
+        nixpkgs.lib.genAttrs systems
+          (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = eachSystem (pkgs:
+        let
+          frida = pkgs.callPackage ./. { };
+        in
+        {
+          inherit (frida)
+            frida-gum
+
+            frida-sdk
+            frida-toolchain
+            ;
+        }
+      );
+    };
+}
