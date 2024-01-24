f12cc26d656c64690209a8c79d54881ec8b98302
itstarsun <itstarsun@proton.me>
2024-01-24 15:35:25 +0000

    16.1.11

--- frida-gumjs-bindings/default.nix
+++ frida-gumjs-bindings/default.nix
@@ -5,5 +5,5 @@ fetchNpmDeps {
     filter = name: type: type == "directory" || !lib.hasSuffix ".nix" (baseNameOf name);
     src = lib.cleanSource ./.;
   };
-  hash = "sha256-6shXsLsfCQdlU0Y0Z0y/rq5BX8+SCvGJOrt/g8TLTCw=";
+  hash = "sha256-c0CpRV37NmSR6wbtX2YfAaWonE5+AdU/9Uo2BYk4PJw=";
 }
